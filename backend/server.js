5c17ca59c0f1e8756b664cc3a087e83a97d44fe3
SAFIKUL HAQ ANSARI <121393287+saf1708@users.noreply.github.com>
2026-03-01 12:09:25 +0530

    Update CORS origin for production deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://todo-full-stack-navy.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
